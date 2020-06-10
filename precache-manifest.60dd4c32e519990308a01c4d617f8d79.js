316718a209b5b9ac1f143b6085b6be99d5c4c5b6
Bianca Herbst <b.herbst@posteo.de>
2020-06-10 21:45:34 +0200

    Updates

---
+++ precache-manifest.60dd4c32e519990308a01c4d617f8d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd84290207b09f4b2057b995d592fc73",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "a9d78e2e1b36d0765bbd",
+    "url": "/random-quote-machine/static/css/main.d008f63a.chunk.css"
+  },
+  {
+    "revision": "63e183794ccc8ef01803",
+    "url": "/random-quote-machine/static/js/2.19848cb9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random-quote-machine/static/js/2.19848cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d78e2e1b36d0765bbd",
+    "url": "/random-quote-machine/static/js/main.acec9c2b.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  },
+  {
+    "revision": "ca110e084857c101a8f9b9e7607c4a14",
+    "url": "/random-quote-machine/static/media/bg.ca110e08.jpg"
+  }
+]);
